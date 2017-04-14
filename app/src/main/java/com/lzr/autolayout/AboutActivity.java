21e775ead56ddf8ec7a74515a3251b839115cff7
kofkoy123 <125032854@qq.com>
2017-04-14 13:51:08 +0800

    更新

--- app/src/main/java/com/lzr/autolayout/AboutActivity.java
+++ app/src/main/java/com/lzr/autolayout/AboutActivity.java
@@ -1,9 +1,10 @@
 package com.lzr.autolayout;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
-public class AboutActivity extends BaseActivity {
+public class AboutActivity extends BaseActivity implements View.OnClickListener{
 
     private ImageView ivBack;
 
@@ -11,5 +12,12 @@ public class AboutActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ivBack = (ImageView) findViewById(R.id.iv_about_back);
+        ivBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        finish();
     }
 }
